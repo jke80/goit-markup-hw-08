58f82982088451f772c6d0b5cd55404c33600cff
Evhenii <kolomoycev81@gmail.com>
2023-05-24 23:26:00 +0300

    modal scroll

--- js/modal.js
+++ js/modal.js
@@ -10,5 +10,13 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+   const isModalOpen =
+      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
+    
+    const scrollLockMethod = !isModalOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body); 
   }
 })();
